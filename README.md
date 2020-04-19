6690805bac186ca8f3034fb7e28382735b1ef0fb
Erin Smith <erin.acumen@gmail.com>
2020-04-18 23:54:29 -0700

    add pass-gen

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # password-gen-homework
 HW #3: Create a dynamic random password generator that takes user input into account
+
+This is a random password generator that populates a password based on prompts answered by the user.  It can contain 8-128 characters, alpha-numeric with capitals and special characters.  This is my first project using more advanced Javascript elements such as: JSON, objects, functions, "for loop", concatination, arrays, forEach, operators, "if else statements",  Math.Random, Math.floor, click events and more.
+
+The generator should validate user inputs, act mobiliy responsive, and render password in text box when prompts are satisfied.  
---
+++ script.js
@@ -0,0 +1,59 @@
+var allChars = [
+  { char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+    name: "Special Characters",
+    use: false },
+  { char: "0123456789",
+    name: "Numbers",
+    use: false },
+  { char: "abcdefghijklmnopqrstuvwxyz",
+    name: "Lowercase Letters",
+    use: false },
+  { char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    name: "Uppercase Letters",
+    use: false},
+]
+var pwdChars = "";
+let password = "";
+
+//GREETING
+alert("Welcome to Magic Password Generator!"); 
+alert ("Follow steps 1 and 2 to Create a Password.");
+//Set default setting on slider toggle
+document.getElementById("dlen").innerHTML = "Length: 50"; 
+//function to move slider number
+document.getElementById("slider").oninput = function() {
+  if(document.getElementById("slider").value > 0){
+    document.getElementById("dlen").innerHTML = "Length: " + document.getElementById("slider").value; //pwdlength
+  }
+  else { 
+    document.getElementById("length").innerHTML = "Length: 8";
+  }
+}
+// Press submit length button to prompt user
+document.getElementById("chars").addEventListener("click", askUser);
+    function askUser () {
+        confirm ("There will be 4 Questions. Please answer yes to one or more:")
+      allChars.forEach(set => {
+        var useChar = (prompt(`Do you want to use ${set.name}?`));
+        if (useChar === "yes" || useChar === "y") {
+            set.use = true;
+        }
+        if(set.use){
+            pwdChars = pwdChars + set.char;
+        }
+      });
+    JSON.stringify(allChars);
+    if (pwdChars !== "") {
+    alert("Please click Generate Password");
+    }
+  else { alert("Error. Please refresh page and answer questions carefully")};
+    document.getElementById("generate").addEventListener("click", generatePassword);
+      function generatePassword() {
+     let plength = document.getElementById("slider").value; 
+        for (var i = 1; i <= plength; i++) {
+          password = password + pwdChars.charAt(Math.floor(Math.random() * Math.floor(pwdChars.length) - 1));
+           document.getElementById("display").innerHTML = password;
+
+        }
+      } 
+    }
