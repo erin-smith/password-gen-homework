f4d288575d5173f0c0170dad230b56e617052fd2
Erin Smith <erin.acumen@gmail.com>
2020-09-13 06:57:53 -0700

    refactored js

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
-var allChars = [
-  { char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+const allChars = [{
+    char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
     name: "Special Characters",
-    use: false },
-  { char: "0123456789",
+    use: false
+  },
+  {
+    char: "0123456789",
     name: "Numbers",
-    use: false },
-  { char: "abcdefghijklmnopqrstuvwxyz",
+    use: false
+  },
+  {
+    char: "abcdefghijklmnopqrstuvwxyz",
     name: "Lowercase Letters",
-    use: false },
-  { char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    use: false
+  },
+  {
+    char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     name: "Uppercase Letters",
-    use: false},
+    use: false
+  },
 ]
-var pwdChars = "";
-let password = "";
+const minLength = 8;
+let pwdChars = "";
 
 //GREETING
-alert("Welcome to Magic Password Generator!"); 
-alert ("Follow steps 1 and 2 to Create a Password.");
-//Set default setting on slider toggle
-document.getElementById("dlen").innerHTML = "Length: 50"; 
-//function to move slider number
-document.getElementById("slider").oninput = function() {
-  if(document.getElementById("slider").value > 0){
-    document.getElementById("dlen").innerHTML = "Length: " + document.getElementById("slider").value; //pwdlength
+alert("Welcome to Magic Password Generator!");
+alert("Follow steps 1 and 2 to Create a Password.");
+
+function updateLength(pwdLength){
+  document.getElementById("dlen").innerHTML = "Length: " + pwdLength;
+}
+
+// align text with slider value
+updateLength(document.getElementById("slider").value);
+
+// Press submit length button to prompt user
+
+function askUser() {
+  confirm("There will be 4 Questions. Please type 'yes' to one or more:")
+  allChars.forEach(set => {
+    const useChar = (prompt(`Do you want to use ${set.name}?`))
+    if (useChar === "yes" || useChar === "y") {
+      pwdChars = pwdChars + set.char;
+    }
+  });
+
+  if (pwdChars !== "") {
+    alert("Please click Generate Password");
+  } else {
+    alert("Error. Please refresh page and answer questions carefully")
+  };
+}
+
+function getRandomUpTo(max){
+  return Math.floor(Math.random() * max);
+}
+
+function generatePassword() {
+  let password = "";
+  let plength = document.getElementById("slider").value;
+  for (let i = 0; i < plength; i++) {
+    password = password + pwdChars.charAt(getRandomUpTo(pwdChars.length));
   }
-  else { 
-    document.getElementById("length").innerHTML = "Length: 8";
+  //console.log(`(${password.length}) ${password}`);
+  document.getElementById("display").innerText = password;
+}
+
+//function to move slider number
+document.getElementById("slider").oninput = function () {
+  if (document.getElementById("slider").value > minLength) {
+    updateLength(document.getElementById("slider").value);
+  } else {
+    updateLength(minLength);
   }
 }
-// Press submit length button to prompt user
-document.getElementById("chars").addEventListener("click", askUser);
-    function askUser () {
-        confirm ("There will be 4 Questions. Please type 'yes' to one or more:")
-      allChars.forEach(set => {
-        var useChar = (prompt(`Do you want to use ${set.name}?`)) 
-        if (useChar === "yes" || useChar === "y") {
-           useChar.toLowerCase;
-            set.use = true;
-        }
-        if(set.use){
-            pwdChars = pwdChars + set.char;
-        }
-      });
-    JSON.stringify(allChars);
-    if (pwdChars !== "") {
-    alert("Please click Generate Password");
-    }
-  else { alert("Error. Please refresh page and answer questions carefully")};
-    document.getElementById("generate").addEventListener("click", generatePassword);
-      function generatePassword() {
-     let plength = document.getElementById("slider").value; 
-        for (var i = 0; i < plength; i++) {
-          password = password + pwdChars.charAt(Math.floor(Math.random() * Math.floor(pwdChars.length)));
-           document.getElementById("display").innerHTML = password;
 
-        }
-      } 
-    }
+document.getElementById("chars").addEventListener("click", askUser);
+document.getElementById("generate").addEventListener("click", generatePassword);
